e002987399dae7ed043a5d24ed1ed4339d0e4859
LuisFranco27 <luis_franco@unitec.edu>
2024-11-22 23:04:15 -0600

    Se agrego imprimirTableroX0

--- Juegos/src/juegos/Juegos.java
+++ Juegos/src/juegos/Juegos.java
@@ -1,23 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package juegos;
 import java.util.Scanner;
-/**
- *
- * @author leona
- */
+
 public class Juegos {
     static Scanner leer = new Scanner(System.in);
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         char [][] tablero = new char [3][3];
         InicializarTablero(tablero);
-        ImprimirTablero(tablero);
+        imprimirTableroX0(tablero);
     }
+    
     public static void InicializarTablero(char[][] tablero) {
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero[i].length; j++) {
@@ -25,18 +17,23 @@ public class Juegos {
             }
         }
     }
-    public static void ImprimirTablero(char[][] tablero) {
-        System.out.println("\n  0 1 2");
-        for (int i = 0; i < tablero.length; i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < tablero[i].length; j++) {
-                System.out.print(tablero[i][j]);
-                if (j < 2) System.out.print("|");
+    
+    // Hecho por Luis Franco
+    public static void imprimirTableroX0(char[][] tablero){
+        for(int i = 0; i < tablero.length; i++){
+            for(int j = 0; j < tablero[i].length; j++){
+                if(j < 2){
+                    System.out.print(" " + tablero[i][j] + " |");
+                }
+                else{
+                    System.out.print(" " + tablero[i][j]);
+                }
             }
             System.out.println();
-            if (i < 2) System.out.println("  -+-+-");
+            if(i < 2){
+                System.out.println("---+---+---");
+            }            
         }
-        System.out.println();
     }
     
     public static boolean Empate(char[][] tablero){
